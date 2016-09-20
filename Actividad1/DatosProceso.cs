52f3812f4d494a80adb1788af24c1ae6fea5485e
Alejandro Frias Ramos <alex.alejandro123@hotmail.com>
2016-09-20 14:26:48 -0500

    comment

--- Actividad1/DatosProceso.cs
+++ Actividad1/DatosProceso.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace Actividad1
 {
     public partial class DatosProceso : Form
-    {
+    {// holi
         public DatosProceso()
         {
             InitializeComponent();
